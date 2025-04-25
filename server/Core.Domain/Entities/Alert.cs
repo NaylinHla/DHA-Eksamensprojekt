c9d2284df5b1d41a2986aa7712954457aa3a3706
Yatokuri <Tho.olesen@gmail.com>
2025-04-25 10:04:29 +0200

    (PATCH) Made some != null

--- server/Core.Domain/Entities/Alert.cs
+++ server/Core.Domain/Entities/Alert.cs
@@ -4,10 +4,10 @@ namespace Core.Domain.Entities;
 
 public class Alert
 {
-    public Guid AlertID { get; set; }
+    public Guid AlertID { get; set; } != null
     public Guid AlertUserId { get; set; }
-    public string AlertName { get; set; }
-    public string AlertDesc { get; set; }
+    public string AlertName { get; set; } != null
+    public string AlertDesc { get; set; } != null
     public DateTime AlertTime { get; set; }
     public Guid? AlertPlant { get; set; }
 
