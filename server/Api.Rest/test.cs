54541fee99c60ba790bf716eb94ac5170e4782d9
Naylin <138529784+NaylinHla@users.noreply.github.com>
2025-04-25 10:19:29 +0200

    Patch: test13213

---
+++ server/Api.Rest/test.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Rest;
+
+public class test
+{
+    public int test_addition(int num1, int num2)
+    {
+
+        int result = num1 + num2;
+        
+        return result;
+
+    }
+
+}
