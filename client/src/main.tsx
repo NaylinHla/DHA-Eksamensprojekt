99a94b48691921b651522b9bcfc7dd24faf18616
Naylin <138529784+NaylinHla@users.noreply.github.com>
2025-04-28 11:25:23 +0200

    fixed lightmode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,9 @@ import {Toaster} from "react-hot-toast";
 import './style.css';
 import {BrowserRouter} from "react-router";
 
+document.documentElement.setAttribute("data-theme", "light");
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-
         <App/>
         <Toaster/>
     </BrowserRouter>,
