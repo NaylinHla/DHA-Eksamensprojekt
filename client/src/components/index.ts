c2e2bca19432c11c5dda4e54957c5a94d3fe47a5
Naylin <138529784+NaylinHla@users.noreply.github.com>
2025-05-06 09:55:15 +0200

    selectable countries baby

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 // ********** Miscellaneous Components **********
 export {default as NavBar} from './Navbar/Navbar';
+export {default as Footer} from './Footer/Footer';
 export {default as ConfirmModal} from './Modals/ConfirmModal';
 export {formatDateTimeForUserTZ} from './utils/DateTime/formatDateTimeForUserTZ ';
 export {useLogout} from './utils/Logout/Logout';
