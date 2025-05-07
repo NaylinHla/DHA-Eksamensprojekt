29c95364c81a91c416c9145bd30a925c2c4e053a
Daniel12345rs01 <danielschultz2001@gmail.com>
2025-05-07 17:00:15 +0200

    Removed unused stuff
    + Fixet some bugs

--- client/src/types/NavLinks.ts
+++ client/src/types/NavLinks.ts
@@ -1,5 +1,5 @@
 export const links = [
-    { to: "/", label: "Dashboard" },
+    { to: "/", label: "DashboardPage" },
     { to: "/plants", label: "Plants" },
     { to: "/alerts", label: "Alerts" },
     { to: "/history", label: "History" },
