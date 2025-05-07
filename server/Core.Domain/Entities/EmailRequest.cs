ae8c79ebfcffc94fb8767d332c7711b7a798736a
Naylin <138529784+NaylinHla@users.noreply.github.com>
2025-05-07 13:41:16 +0200

    request change

--- server/Core.Domain/Entities/EmailRequest.cs
+++ server/Core.Domain/Entities/EmailRequest.cs
@@ -2,7 +2,6 @@
 
 public class EmailRequest
 {
-    public string Email { get; set; }  = null!;
     public string Subject { get; set; }  = null!;
     public string Message { get; set; }  = null!;
 }
