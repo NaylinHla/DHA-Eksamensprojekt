4fc1d8d4caf8578618c97e3fa0e606f964774c47
OliSparrow <aceywaceywake@outlook.com>
2025-05-08 09:30:05 +0200

    Fixed Dashboard name

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,4 +1,4 @@
-export {default as DashboardPage} from './Dashboard/DashBoard';
+export {default as DashboardPage} from './Dashboard/DashboardPage';
 export {default as HistoryPage} from './History/HistoryPage';
 export {default as AlertPage} from './Alert/AlertOverview'
 
