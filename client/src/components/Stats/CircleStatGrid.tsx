6d20894d24fb445f7d78a1a02d573ad41a3ab3af
Daniel12345rs01 <danielschultz2001@gmail.com>
2025-05-22 14:10:08 +0200

    Dashboard now show the device with newest data and name
    + Device circle now always is inside box

--- client/src/components/Stats/CircleStatGrid.tsx
+++ client/src/components/Stats/CircleStatGrid.tsx
@@ -2,11 +2,7 @@
 
 export const CircleStatGrid: React.FC<React.PropsWithChildren> = ({ children }) => (
     <div
-        className="
-      grid gap-x-8 gap-y-6
-      sm:grid-cols-[repeat(auto-fit,minmax(11rem,1fr))]
-      place-items-center
-    "
+        className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-6 w-full max-w-full place-items-center"
     >
         {children}
     </div>
