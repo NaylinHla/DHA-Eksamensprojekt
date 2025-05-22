a47ab4e4255ab885ae1361f4051b4844beb2bfd8
Yatokuri <Tho.olesen@gmail.com>
2025-05-22 13:14:22 +0200

    Updated Mutation testing - Hopefully it can find the config as well

--- server/Startup.Tests/Auth/SecurityServiceTests.cs
+++ server/Startup.Tests/Auth/SecurityServiceTests.cs
@@ -27,20 +27,19 @@ public class SecurityServiceTests
     [TearDown]
     public void TearDown()
     {
-        _httpClient?.Dispose();
+        _httpClient.Dispose();
     }
 
     [Test]
     public Task Hash_Can_Correctly_Hash_A_String()
     {
-        var expected =
-            "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86";
+        const string expected = "b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86";
         var securityService = _scopedServiceProvider.GetRequiredService<ISecurityService>();
         var hash = securityService.HashPassword("password");
         if (hash != expected)
             throw new Exception("Did not create the expected SHA512 hash. Got: " + hash + " and expected: " +
                                 expected);
-        Assert.That(hash, Is.EqualTo(expected));;
+        Assert.That(hash, Is.EqualTo(expected));
         return Task.CompletedTask;
         
     }
